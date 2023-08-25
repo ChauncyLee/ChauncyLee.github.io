14ea139a744024de80800549fb1503133554c8db
阿杰 <53169820+laihaojie@users.noreply.github.com>
2023-08-25 12:02:14 +0800

    chore: update

--- scripts/push.ts
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process'
-
-execSync('npm run build', { stdio: 'inherit' })
-
-execSync('git add .', { stdio: 'inherit' })
-execSync('git commit -m "update"', { stdio: 'inherit' })
-execSync('git push', { stdio: 'inherit' })
-execSync('git push --tags', { stdio: 'inherit' })
